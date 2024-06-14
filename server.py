85da3ae37680199eb79eaa8546667322d7398947
novrmd <135973466+novrmd@users.noreply.github.com>
2024-06-14 22:15:26 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 from flask import Flask, request, jsonify
 import pandas as pd
 from datetime import datetime
@@ -63,5 +62,4 @@ def post_data():
     })
 
 if __name__ == "__main__":
-    # Run the Flask app on all available IP addresses (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port=5000, debug=True)
